10934d7cac5458dfcc350dafed021872ca1f7cda
Lahely Sabrina <lahelyna75@gmail.com>
2022-02-23 18:57:49 +0100

    bon fonctionnement local storage, mentor Jean-Baptiste

--- front/js/product.js
+++ front/js/product.js
@@ -1,91 +1,115 @@
-
-/*Rechercher l'id dans le navigateur window*/
+//Rechercher l'id dans le navigateur window
 const queryString = window.location.search;
 const urlSearchParams = new URLSearchParams(queryString);
 let id = urlSearchParams.get("id");
 
 
-
-//let responseAutre = fetch(`http://localhost:3000/api/products/${id}`);
-
-fetch(`http://localhost:3000/api/products/${id}`) // => On va chercher le detail du canapé selectionné
+fetch(`http://localhost:3000/api/products/` + id)// => On va chercher (dans l'API) le détail du canapé sélectionné.
 .then((response)=> response.json())
 .then((canap)=>{
     console.log(canap)
-    //Ajout de l'image, création de la balise puis ajouter à son élement parent
+    //Ajout de l'image, créer la balise puis l'ajouter à son élément parent(div item_img).
    let image = document.createElement('img');
    image.src = `${canap.imageUrl}`
    image.alt = `${canap.altTxt}`
-   document.querySelector(".item__img").appendChild(image);
+   document.querySelector(".item__img").appendChild(image);//déplace(app), en tant que (child), à la fin(end)
 
    // Ajout de la valeur dans la balise H1
    document.getElementById("title").innerHTML = canap.name;
-   //Ajout du prix
-   document.getElementById ("price").innerHTML = canap.price;
-   //Ajout de la description
+   //Ajout du prix dans la balise span
+   document.getElementById("price").innerHTML = canap.price;
+   //Ajout de la description dans la balise p
    document.getElementById("description").textContent = canap.description;
 
 
-   // couleurs
+   // Couleurs : création de la variable en tant que parent avec sa boucle (conditions)et ses options; Ajouter son enfant (la variable).
    let colorSelect = document.getElementById("colors");
-   
+
    for (let i = 0; i < canap.colors.length; i++){
-       let opt = document.createElement('option');
+       let opt = document.createElement('option');//déclarer la variable opt
        opt.value = canap.colors[i];
        opt.innerHTML = canap.colors[i];
 
        colorSelect.appendChild(opt)
     }
-   
-
-  
     })
-    
-document.getElementById("addToCart").addEventListener('click', (e)=>{
-    //ici enregistrer l'article dans le localstorage
-    console.log("houhouhohu")
-})
+
+    //let button = document.getElementById("addToCart");
+    document.getElementById("addToCart").addEventListener('click', (e)=>{//(type, fonction)//ici enregistrer l'article dans le localstorage
+    let quantityProduct = document.getElementById('quantity');
+    let colorProduct = document.getElementById('colors');
+        let product = {
+            id : id,
+            color : colorProduct.value,//
+            quantity : parseInt(quantityProduct.value) //une chaine de caractères en nombres
+        }
+
+        console.log(product)
+        addBasket(product)
+
+    // let basket = getBasket();// récupère le panier
+
+
+    //  let foundProduct = basket.find(p=>p.id==product.id);//F° find cherche dans un tableau avec 1 condition
+    //  if (foundProduct != undefined){
+    //      foundProduct.quantity++; //ajouter
+    //  }else{
+    //     product.quantity = 1;
+    //     basket.push(product);
+    // }
+    //     saveBasket(basket);//récup le nouv panier
+    })
+
+    //document.getElementById("cart__items").innerHTML = basket;
+function saveBasket(basket){// 1) enregistrer (créer) un panier;nom de la fonction + paramètre(quel panier enregistré)
+    localStorage.setItem("basket", JSON.stringify(basket));//clé, valeur.
+}//Ds le local storage, on ne peut enregistrer que des chaines de caractères/variables=>sérialisation
+//method JSON.stringify : transf données complexes en chaine de caractères
+
+function getBasket(){// 2) récupérer un élém
+    let basket = localStorage.getItem("basket");//variable
+    if(basket == null){
+        return[];//tableau vide ok
+    }else{
+        return JSON.parse(basket);
+        //return JSON.parse(localStorage.getItem("basket"));//method JSON.parse transforme string en objets, tabl, données complexes.("clé")
+    }
+}
+
+function addBasket(product){// 3) le panier considéré comme un tableau ok. (product{product-ID}, {product-color})
+    let basket = getBasket();// récupère le panier
+    let foundProduct = basket.find(p=>p.id==product.id && p.color === product.color);//F° find cherche dans un tableau avec 1 condition
+    if (foundProduct != undefined){ //là il trouve le produit        
+        foundProduct.quantity = foundProduct.quantity + product.quantity //ajouter
+    }else{
+        //product.quantity = 1;
+        basket.push(product);
+    }
+        saveBasket(basket);//récup le nouv panier
+}
+
 
 
 
 /*console.log(id)
 fetch(`http://localhost:3000/api/products/` + id)
-fetch(`http://localhost:3000/api/products/${id}`)
+ //let responseAutre = fetch(`http://localhost:3000/api/products/${id}`);
     .then((data) => data.json({}))
     .then((canap)=>console.log(canap.name))
-    .then(canap => {
-        let affichage = '';
-        for (let id of canap) {
-            affichage += `<a href="./product.html?id=${canap._id}">
-        <article>
-          <img src="${canap.imageUrl}" alt="${canap.altTxt}">
-          <h1 id="title">${canap.name}</h1>
-          <p>Prix : <span id="price">${canap.price}</span>€</p>
-          <p id="description">${canap.description}</p> 
-          
-          <select name="color-select" id="colors">
-              <option value=""></option>
-
-          </select>
-          
-
-        </article>
-      </a>`
-        }
 
         let colors = [i];
             for (let i = 0, i <= colors.lenght, i += 1) {
                 console.log([i])
             }
-            default
+
 }
 document.querySelector(#"nbArticle").addEventListener('input', function(){
     if(this.value)
 
 });
         let nbArticle = 0;
-        
-        consolole.log(urlParam[0])
 
-console.log(id)
-fetch(`http://localhost:3000/api/products/`+id)*/
+        consolole.log(urlParam[0])
+*/
+
+
